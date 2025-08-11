ca60066a64b11e4e797d5e2f29a175812f9013bd
bibhupaulranjan-cmd <bibhupaulranjan@gmail.com>
2025-08-11 21:44:05 +0530

    Add files via upload

---
+++ substraction of two numbers.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num1,num2,substraction;
+	printf("enter first number:");
+	scanf("%d",&num1);
+	printf("enter 2nd number:");
+	scanf("%d",&num2);
+	substraction=num1-num2
+	;printf("sum=%d\n",substraction);
+	return 0;
+}
